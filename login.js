8b450b68859f788290f53dfc81c63514096bb445
hanyarekayasa <132421544+fatihad0@users.noreply.github.com>
2026-07-29 23:53:32 +0700

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-// Daftar Akun Pengguna (Username & Password bisa kamu ganti di sini)
-const daftarUser = [
-    { username: "admin", password: "admin", role: "Administrator" },
-    { username: "petugas1", password: "abc", role: "Staff Gudang" },
-    { username: "kepala", password: "789", role: "Pimpinan" }
-];
